8859dee11fdab0434ac6ac3242c6834591396521
Arkadi <arkadi.schelling@smartb.eu>
2015-07-26 16:25:46 +0200

    Some refactoring and further planning of the argspan labeler

--- zwitscher/learn_sentdist.py
+++ zwitscher/learn_sentdist.py
@@ -3,59 +3,16 @@
 """
 A script to learn classification of sentence distance between connective arguments
 """
-import pickle
 import os
 import uuid
+import pickle
 
-import numpy as np
-import pandas as pd
-
-from sklearn.preprocessing import LabelEncoder
-from sklearn.cross_validation import cross_val_score
-from sklearn.ensemble import RandomForestClassifier
-
-from utils.PCC import load_connectors as load_pcc
 from gold_standard import pcc_to_gold
-from features import discourse_connective_text_featurizer
+from learn import load_data, clean_data, learn_sentdist
 
 __author__ = 'arkadi'
 
-
-def load_data(connector_folder='/media/arkadi/arkadis_ext/NLP_data/ger_twitter/' +
-                               'potsdam-commentary-corpus-2.0.0/connectors'):
-    # Load PCC data
-    pcc = load_pcc(connector_folder)
-    # creating a pd.DataFrame
-    return pcc_to_gold(pcc)
-
-def clean_data(dataframe):
-    """ Transforms all Nones int np.NaN and drops rows where connective_position or sentences is NaN
-
-    :param dataframe: Gold data from the PCC
-    :type dataframe: pd.DataFrame
-    :return: Cleaned data
-    :rtype: pd.DataFrame
-    """
-    dataframe[dataframe.isnull()] = np.NaN
-    dataframe = dataframe.dropna(subset=['connective_positions', 'sentences'])
-    return dataframe
-
-def featurize_data(dataframe, feature_function):
-    """ Creates features as specified by the featurizer that creates a feature dict from
-    sentences and connective positions
-
-    Sentences are a list of lists with tokens
-    Connective positions is a list with pairs of sentence and token indices
-    :param dataframe: data with columns 'sentences' and 'connective_positions'
-    :type dataframe: pd.DataFrame
-    :return: features
-    :rtype: pd.DataFrame
-    """
-    features = dataframe.apply(lambda row: pd.Series(feature_function(row['sentences'], row['connective_positions'])),
-                               axis=1, reduce=False)
-    return features
-
-
+# ToDo: Clickify this!
 def main(feature_list=['connective_lexical', 'length_connective',
                        'length_prev_sent', 'length_same_sent', 'length_next_sent',
                        'tokens_before', 'tokens_after', 'tokens_between'],
@@ -64,10 +21,17 @@ def main(feature_list=['connective_lexical', 'length_connective',
                           'potsdam-commentary-corpus-2.0.0/connectors',
          pickle_folder='data',
          unpickle_gold=True,
-         pickle_classifier=True):
+         pickle_classifier=True
+    ):
+
+
     """ The main learning function for a classifier
 
-    Runs a random forest. Prints out scores from a 5-fold cross validation. Returns the classifier.
+    Runs a random forest. Prints out accuracy scores from a 5-fold cross validation.
+    Returns the classifier and the label encoder that was used.
+    :param feature_list: list of features that shall be calculated with discourse_connective_text_featurizer
+    :param label_features: list of features that have to be encoded as labels
+    :return: trained classifier, score array and label encoder
     :param connector_folder: folder with the gold data. Can be left empty when unpickling the gold data
     :type connector_folder: str
     :param pickle_folder: The folder where pickles are put. The names are dealt with internally
@@ -102,31 +66,7 @@ def main(feature_list=['connective_lexical', 'length_connective',
     clean_pcc = clean_data(pcc_df)
     print 'Cleaned data'
 
-    print 'Calculating features...'
-    # Taking our favorite featurizer
-    featurizer = lambda sents, conn_pos: discourse_connective_text_featurizer(sents, conn_pos,
-                                                                              feature_list=feature_list)
-    features = featurize_data(clean_pcc, featurizer)  # Got features of X
-    print 'Calculated all features'
-
-    # We need to encode the non-numerical labels
-    le = LabelEncoder()
-    if label_features:
-        print 'Encoding labels...'
-        # LabelEncoder only deals with 1 dim np.arrays
-        le.fit(features[label_features].values.ravel())
-        for feat in label_features:
-            features[feat] = le.transform(features[feat])
-        print 'Encoded label'
-
-    print 'Cross validating classifier...'
-    clf = RandomForestClassifier(min_samples_leaf=5, n_jobs=-1, verbose=1)
-    scores = cross_val_score(clf, features, clean_pcc['sentence_dist'], cv=5)
-    print 'Cross validated classifier\nscores: %s\nmean score: %f' % (str(scores), scores.mean())
-
-    print 'Learning classifier on the whole data set...'
-    clf.fit(features, clean_pcc['sentence_dist'])
-    print 'Learned classifier on the whole data set'
+    clf, scores, le = learn_sentdist(clean_pcc, feature_list=feature_list, label_features=label_features)
 
     if pickle_classifier:
         classifier_folder = os.path.join(pickle_folder, 'classifiers/')
@@ -141,7 +81,7 @@ def main(feature_list=['connective_lexical', 'length_connective',
             pickle.dump(clf, f, pickle.HIGHEST_PROTOCOL)
         with open(os.path.join(classifier_folder, 'classifier.log'), 'a') as f:
             f.write('%s\t%f\t%s\n' % (id, scores.mean(), str(feature_list)))
-    return clf, le
+
 
 if __name__ == '__main__':
 
